9059df2ac918d432dba9a799c76722e9fc8f1e57
rohitmi0023 <rohitracer0023@gmail.com>
2019-08-28 22:49:57 +0530

    deploy

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use('/api/auth', require('./routes/api/auth'));
 //Serve static assests if in production
 if (process.env.NODE_ENV === 'production') {
 	//Set static folder
-	app.use(express.static('client/build'));
+	app.use(express.static(path.join(__dirname, 'client/build')));
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
